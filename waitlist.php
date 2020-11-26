cff3832a64f19ff44d8424fdb6e89737aaaf00b9
Josh Choi <joshchoi4881@gmail.com>
2020-11-25 19:39:43 -0500

    Added Google Analytics

--- waitlist.php
+++ waitlist.php
@@ -31,7 +31,16 @@
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <!-- Global site tag (gtag.js) - Google Analytics 1 -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-138974831-2"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-138974831-2');
+        </script>
+        <!-- Global site tag (gtag.js) - Google Analytics 2 -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-4B3QS6PGZN"></script>
         <script>
             window.dataLayer = window.dataLayer || [];

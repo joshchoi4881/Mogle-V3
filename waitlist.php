d3c37122dd8738464e4559a24a3f354b2565aa07
Josh Choi <joshchoi4881@gmail.com>
2020-12-16 15:26:08 -0500

    Small bug fix

--- waitlist.php
+++ waitlist.php
@@ -21,6 +21,10 @@
         <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
         <link href="images/favicon.png" rel="shortcut icon" type="image/x-icon">
         <link href="images/webclip.png" rel="apple-touch-icon">
+        <!-- Bootstrap -->
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <!-- Global site tag (gtag.js) - Google Analytics 1 -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-138974831-2"></script>
         <script>

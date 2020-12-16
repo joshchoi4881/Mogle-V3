5418ea48ed8c1a2e2ef9b73425236cdcebde4467
Josh Choi <joshchoi4881@gmail.com>
2020-12-16 14:12:30 -0500

    Minor design changes

--- scheduler.php
+++ scheduler.php
@@ -8,6 +8,8 @@
         <meta name="keywords" content="Mogle">
         <meta name="author" content="Josh Choi">
         <title>Mogle</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport">
+        <meta content="Webflow" name="generator">
         <link href="css/mogle.css" rel="stylesheet" type="text/css"/>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
         <link href="css/normalize.css" rel="stylesheet" type="text/css">
@@ -19,10 +21,6 @@
         <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
         <link href="images/favicon.png" rel="shortcut icon" type="image/x-icon">
         <link href="images/webclip.png" rel="apple-touch-icon">
-        <!-- Bootstrap -->
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <!-- Global site tag (gtag.js) - Google Analytics 1 -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-138974831-2"></script>
         <script>
@@ -44,22 +42,22 @@
     </head>
     <body>
         <header id="nav" class="sticky-nav">
-            <nav class="w-container">
+            <div>
                 <ul role="list" class="nav-grid w-list-unstyled">
                     <li id="w-node-f042de250a67-0b8a3ee6" class="list-item">
-                        <a href="index.html" class="nav-logo-link w-inline-block"><img src="images/Mogle-Logo.png" sizes="(max-width: 479px) 153.796875px, 156.40625px" srcset="images/Mogle-Logo-p-500.png 500w, images/Mogle-Logo-p-800.png 800w, images/Mogle-Logo-p-1080.png 1080w, images/Mogle-Logo-p-1600.png 1600w, images/Mogle-Logo-p-2000.png 2000w, images/Mogle-Logo-p-2600.png 2600w, images/Mogle-Logo.png 3582w" alt="" class="nav-logo"></a>
+                        <a href="index.html" class="nav-logo-link w-inline-block"><img src="images/Mogle-Logo-White.png" sizes="(max-width: 479px) 153.796875px, 156.40625px" srcset="images/Mogle-Logo-White-p-500.png 500w, images/Mogle-Logo-White-p-800.png 800w, images/Mogle-Logo-White-p-1080.png 1080w, images/Mogle-Logo-White-p-1600.png 1600w, images/Mogle-Logo-White-p-2000.png 2000w, images/Mogle-Logo-White-p-2600.png 2600w, images/Mogle-Logo-White.png 3582w" alt="" class="nav-logo"></a>
                     </li>
                     <li class="list-item">
                         <a href="analytics.php" class="nav-link">Analytics</a>
                     </li>
-                    <li class="list-item">
+                    <li>
                         <a href="scheduler.php" class="nav-link">Scheduler</a>
                     </li>
-                    <li class="list-item">
+                    <li>
                         <a href="waitlist.php" class="nav-link">Join Waitlist</a>
                     </li>
                 </ul>
-            </nav>
+            </div>
         </header>
         <section class="center">
             <div class="card">
--- waitlist.php
+++ waitlist.php
@@ -8,29 +8,19 @@
         <meta name="keywords" content="Mogle">
         <meta name="author" content="Josh Choi">
         <title>Mogle</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport">
+        <meta content="Webflow" name="generator">
         <link href="css/mogle.css" rel="stylesheet" type="text/css"/>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
         <link href="css/normalize.css" rel="stylesheet" type="text/css">
         <link href="css/webflow.css" rel="stylesheet" type="text/css">
         <link href="css/mogle.webflow.css" rel="stylesheet" type="text/css">
-        <style>
-            label {
-                display: inline-block;
-            }
-            .description {
-                text-indent: 50px;
-            }
-        </style>
         <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
         <script type="text/javascript">WebFont.load({  google: {    families: ["Ovo:regular"]  }});</script>
         <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
         <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
         <link href="images/favicon.png" rel="shortcut icon" type="image/x-icon">
         <link href="images/webclip.png" rel="apple-touch-icon">
-        <!-- Bootstrap -->
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <!-- Global site tag (gtag.js) - Google Analytics 1 -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-138974831-2"></script>
         <script>
@@ -52,22 +42,22 @@
     </head>
     <body>
         <header id="nav" class="sticky-nav">
-            <nav class="w-container">
-            <ul role="list" class="nav-grid w-list-unstyled">
-                <li id="w-node-f042de250a67-0b8a3ee6" class="list-item">
-                <a href="index.html" class="nav-logo-link w-inline-block"><img src="images/Mogle-Logo.png" sizes="(max-width: 479px) 153.796875px, 156.40625px" srcset="images/Mogle-Logo-p-500.png 500w, images/Mogle-Logo-p-800.png 800w, images/Mogle-Logo-p-1080.png 1080w, images/Mogle-Logo-p-1600.png 1600w, images/Mogle-Logo-p-2000.png 2000w, images/Mogle-Logo-p-2600.png 2600w, images/Mogle-Logo.png 3582w" alt="" class="nav-logo"></a>
-                </li>
-                <li class="list-item">
-                <a href="analytics.php" class="nav-link">Analytics</a>
-                </li>
-                <li class="list-item">
-                <a href="scheduler.php" class="nav-link">Scheduler</a>
-                </li>
-                <li class="list-item">
-                <a href="waitlist.php" class="nav-link">Join Waitlist</a>
-                </li>
-            </ul>
-            </nav>
+            <div>
+                <ul role="list" class="nav-grid w-list-unstyled">
+                    <li id="w-node-f042de250a67-0b8a3ee6" class="list-item">
+                        <a href="index.html" class="nav-logo-link w-inline-block"><img src="images/Mogle-Logo-White.png" sizes="(max-width: 479px) 153.796875px, 156.40625px" srcset="images/Mogle-Logo-White-p-500.png 500w, images/Mogle-Logo-White-p-800.png 800w, images/Mogle-Logo-White-p-1080.png 1080w, images/Mogle-Logo-White-p-1600.png 1600w, images/Mogle-Logo-White-p-2000.png 2000w, images/Mogle-Logo-White-p-2600.png 2600w, images/Mogle-Logo-White.png 3582w" alt="" class="nav-logo"></a>
+                    </li>
+                    <li class="list-item">
+                        <a href="analytics.php" class="nav-link">Analytics</a>
+                    </li>
+                    <li>
+                        <a href="scheduler.php" class="nav-link">Scheduler</a>
+                    </li>
+                    <li>
+                        <a href="waitlist.php" class="nav-link">Join Waitlist</a>
+                    </li>
+                </ul>
+            </div>
         </header>
         <section class="center">
             <div class="card">
@@ -85,44 +75,44 @@
                 <br/>
                 <div class="left">
                     <input type="checkbox" id="newsletter" name="newsletter" value="Newsletter">
-                    <label class="gilroy" for="newsletter"> Newsletter: we'll email you three new freelance opportunities every week, as well as freelance-related news.</label>
+                    <label class="label gilroy" for="newsletter"> Newsletter: we'll email you three new freelance opportunities every week, as well as freelance-related news.</label>
                     <br/>
                     <input type="checkbox" id="analytics" name="analytics" value="Analytics">
-                    <label class="gilroy" for="analytics"> <a href="analytics.php">Analytics</a>: we'll analyze your earnings data and help you find patterns in your income to optimize it.</label>
+                    <label class="label gilroy" for="analytics"> <a href="analytics.php">Analytics</a>: we'll analyze your earnings data and help you find patterns in your income to optimize it.</label>
                     <br>
                     <input type="checkbox" id="scheduler" name="scheduler" value="Scheduler">
-                    <label class="gilroy" for="scheduler"> <a href="scheduler.php">Scheduler</a>: we'll start conducting wage insurance in your city.</label>
+                    <label class="label gilroy" for="scheduler"> <a href="scheduler.php">Scheduler</a>: we'll start conducting wage insurance in your city.</label>
                 </div>
                 <br>
                 <p class="gilroy">Which platforms do you currently use?</p>
                 <br/>
                 <div class="left">
                     <input type="checkbox" id="Uber" name="Uber" value="Uber">
-                    <label class="gilroy" for="Uber"> Uber</label>
+                    <label class="label gilroy" for="Uber"> Uber</label>
                     <br/>
                     <input type="checkbox" id="Lyft" name="Lyft" value="Lyft">
-                    <label class="gilroy" for="Lyft"> Lyft</label>
+                    <label class="label gilroy" for="Lyft"> Lyft</label>
                     <br>
                     <input type="checkbox" id="Uber_Eats" name="Uber_Eats" value="Uber_Eats">
-                    <label class="gilroy" for="Uber_Eats"> Uber Eats</label>
+                    <label class="label gilroy" for="Uber_Eats"> Uber Eats</label>
                     <br>
                     <input type="checkbox" id="Postmates" name="Postmates" value="Postmates">
-                    <label class="gilroy" for="Postmates"> Postmates</label>
+                    <label class="label gilroy" for="Postmates"> Postmates</label>
                     <br>
                     <input type="checkbox" id="DoorDash" name="DoorDash" value="DoorDash">
-                    <label class="gilroy" for="DoorDash"> DoorDash</label>
+                    <label class="label gilroy" for="DoorDash"> DoorDash</label>
                     <br>
                     <input type="checkbox" id="Grubhub" name="Grubhub" value="Grubhub">
-                    <label class="gilroy" for="Grubhub"> Grubhub</label>
+                    <label class="label gilroy" for="Grubhub"> Grubhub</label>
                     <br>
                     <input type="checkbox" id="Instacart" name="Instacart" value="Instacart">
-                    <label class="gilroy" for="Instacart"> Instacart</label>
+                    <label class="label gilroy" for="Instacart"> Instacart</label>
                     <br>
                     <input type="checkbox" id="Shipt" name="Shipt" value="Shipt">
-                    <label class="gilroy" for="Shipt"> Shipt</label>
+                    <label class="label gilroy" for="Shipt"> Shipt</label>
                     <br>
                     <input type="checkbox" id="other" name="other" value="other">
-                    <label class="gilroy" for="other"> Other</label>
+                    <label class="label gilroy" for="other"> Other</label>
                     <br/>
                     <div id="otherDiv" style="display:none;">
                         <p class="gilroy">Other: </p>
